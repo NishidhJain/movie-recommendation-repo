f2631be96baccc78249b7370acdee1ae0b505b80
NishidhJain <nishidhjain909@gmail.com>
2021-03-21 13:00:43 +0530

    deleted some files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import './App.css';
-import Movies from './components/Movies';
 
 function App() {
-	return (
-		<div className='app'>
-			<Movies />
-		</div>
-	);
+	return <div className='app'>Movie Recommendar</div>;
 }
 
 export default App;
-
-// API Key
-// e8c83f7123dd47102c59318c8f456d8d
-
-// Search API
-// https://api.themoviedb.org/3/search/movie?api_key={key}&language=en-US&query=name&page=1&include_adult=false
-
-// Img API
-// https://image.tmdb.org/t/p/w1280/{posterPath}
