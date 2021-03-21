3b0ab76e279ffcb38a87c7a3ac48aeb30beb6307
NishidhJain <nishidhjain909@gmail.com>
2021-03-21 14:37:05 +0530

    added routing to different apges

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { MovieProvider } from './context/Context';
+import Dashboard from './pages/Dashboard';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import './App.css';
 
 function App() {
 	return (
 		<MovieProvider>
+			<Router>
+				<Switch>
+					<Route path='/' exact>
+						<Dashboard />
+					</Route>
+					<Route path='/signin' exact>
+						<SignIn />
+					</Route>
+					<Route path='/signup' exact>
+						<SignUp />
+					</Route>
+				</Switch>
+			</Router>
 			<div className='app'>Movie Recommendar</div>
 		</MovieProvider>
 	);
