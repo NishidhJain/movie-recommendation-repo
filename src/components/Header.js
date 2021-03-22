e6b2ba1dac663c49389dced12f2c2e4e45b520e6
NishidhJain <nishidhjain909@gmail.com>
2021-03-22 21:36:35 +0530

    changed some styling of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,22 +4,24 @@ import { Link } from 'react-router-dom';
 function Header() {
 	return (
 		<header className='header'>
-			<h1 className='header__logo'>Moviestan</h1>
+			<div className='header__container'>
+				<h1 className='header__logo'>Moviestan</h1>
 
-			<nav className='header__nav'>
-				<ul className='header__list'>
-					<li className='header__listItem'>
-						<Link to='/search' className='header__link'>
-							Search
-						</Link>
-					</li>
-					<li className='header__listItem'>
-						<Link to='/profile' className='header__link'>
-							Profile
-						</Link>
-					</li>
-				</ul>
-			</nav>
+				<nav className='header__nav'>
+					<ul className='header__list'>
+						<li className='header__listItem'>
+							<Link to='/search' className='header__link'>
+								Search
+							</Link>
+						</li>
+						<li className='header__listItem'>
+							<Link to='/profile' className='header__link'>
+								Profile
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	);
 }
